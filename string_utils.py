b8870daa121b9e8f920ec3ef4d48f029546ca723
noamsalo <noamsal30@gmail.com>
2025-11-14 13:45:05 +0200

    Add split_at_first_digit function implementation
    Implement split_at_first_digit function to split a string at the first digit.

--- string_utils.py
+++ string_utils.py
@@ -3,4 +3,10 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+  digit_loction = 1
+  for i in range(len(formula)):
+    if formula[i].isdigit():
+      digit_location = i
+      break
+  return(formula[0:digit_location], formula[digit_location:])
+
